55a738083652442887880841ff1b190e493853f7
Chris Wray <christov@christov.net>
2025-05-24 18:14:00 +0100

    steamos 3.7.8 fix

--- defaults/run_kde_connect.sh
+++ defaults/run_kde_connect.sh
@@ -4,4 +4,4 @@ export XDG_RUNTIME_DIR=/run/user/1000
 export SHELL=/bin/bash
 export HOME=/home/deck
 export DISPLAY=$1
-sudo -u deck -E /usr/lib/kdeconnectd --replace
+sudo -u deck -E kdeconnectd --replace
